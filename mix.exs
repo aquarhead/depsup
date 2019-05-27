98d3ef35620100e60e8053ce563df2f0ade37528
LOU Xun <aquarhead@ela.build>
2019-05-27 13:50:39 +0000

    Add dialyxir

--- mix.exs
+++ mix.exs
@@ -7,11 +7,32 @@ defmodule DepSup.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: [{:ex_doc, "~> 0.20", only: :dev, runtime: false}]
+      deps: deps(),
+
+      # Docs
+      name: "DepSup",
+      source_url: "https://github.com/aquarhead/depsup",
+      docs: docs()
     ]
   end
 
   def application do
-    []
+    [
+      mod: {DepSup.Application, []}
+    ]
+  end
+
+  defp deps() do
+    [
+      {:dialyxir, "~> 1.0.0-rc.6", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.20", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
+    ]
   end
 end
